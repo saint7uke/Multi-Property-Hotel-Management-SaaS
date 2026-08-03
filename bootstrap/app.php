606f49d106b65a9295e9c94a43b34d1b816ab68f
saint.7uke <lucasisaiah.bargo@cvsu.edu.ph>
2026-08-03 09:50:34 +0800

    Trust Vercel HTTPS proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        if (getenv('VERCEL')) {
+            $middleware->trustProxies(at: '*');
+        }
+
         $middleware->statefulApi();
         $middleware->throttleApi();
 
