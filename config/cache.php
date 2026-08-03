25141877fbe6cf78768616cd83d3d4c00919ead2
saint.7uke <lucasisaiah.bargo@cvsu.edu.ph>
2026-08-03 20:34:44 +0800

    Harden Vercel runtime defaults

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_STORE', 'database'),
+    'default' => env('VERCEL') ? 'array' : env('CACHE_STORE', 'database'),
 
     'limiter' => env(
         'CACHE_LIMITER',
--- config/session.php
+++ config/session.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'driver' => env('SESSION_DRIVER', 'database'),
+    'driver' => env('VERCEL') ? 'cookie' : env('SESSION_DRIVER', 'database'),
 
     /*
     |--------------------------------------------------------------------------
